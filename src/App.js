f8c5d0eb2b6fdf707797fdfd1761a6dab20c239f
sujunglee <sujunglee2020@u.northwestern.edu>
2019-02-18 02:32:33 -0600

    Implement basic tier1 and tier2 highlighting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { exampleA, exampleB } from "./Components/data";
+import Highlighter from "react-highlight-words";
+import Button from "@material-ui/core/Button";
 
-class App extends Component {
+// Parse the CSS code into arrays
+var exampleAarr = exampleA.split("\n");
+var exampleBarr = exampleB.split("\n");
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isSelected: [exampleAarr, exampleBarr], highlight: [] };
+    this.highlightCode = this.highlightCode.bind(this);
+  }
+
+  highlightCode(tier) {
+    var examples = this.state.isSelected;
+    if (tier === true) {
+      var tier1 = examples.reduce((a, b) =>
+        a.filter(c => b.includes(c.trim()))
+      );
+      this.setState(state => {
+        return { highlight: tier1 };
+      });
+    } else {
+      var i;
+      var all_attributes = [];
+      for (i = 0; i < examples.length; i++) {
+        var j;
+        var curr_code = [];
+        for (j = 0; j < examples[i].length; j++) {
+          var temp = examples[i][j].split(":");
+          if (temp.length > 0) {
+            // Store only the attributes
+            curr_code.push(temp[0]);
+          }
+          all_attributes.push(curr_code);
+        }
+        var tier2 = all_attributes.reduce((a, b) =>
+          a.filter(c => b.includes(c.trim()))
+        );
+        console.log(tier2);
+      }
+      this.setState(state => {
+        return { highlight: tier2 };
+      });
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        <div id="exampleA">
+          <Highlighter
+            className="HighlightText"
+            searchWords={this.state.highlight}
+            textToHighlight={exampleA}
+          />
+        </div>
+        <br />
+        <br />
+        <div id="exampleB">
+          <Highlighter
+            className="HighlightText"
+            searchWords={this.state.highlight}
+            textToHighlight={exampleB}
+          />
+        </div>
+        <br />
+        <div className="Buttons">
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => this.highlightCode(true)}
           >
-            Learn React
-          </a>
-        </header>
+            Tier 1 Highlighting
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => this.highlightCode(false)}
+          >
+            Tier 2 Highlighting
+          </Button>
+        </div>
       </div>
     );
   }
---
+++ src/Components/data.js
@@ -0,0 +1,4 @@
+export var exampleA =
+  "position: absolute;\nposition: fixed;\nbottom: 0px;\nright: 50px;";
+
+export var exampleB = "position: fixed;\nright: 6px;\ntop: 50%;\nz-index: 1;";
